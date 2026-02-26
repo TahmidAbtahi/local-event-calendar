adfec341b708a2647d8696ab36561e942559c7bd
TahmidAbtahi <abtahi1@umbc.edu>
2026-02-26 13:04:02 -0500

    Change revalidation to force-dynamic
    Updated revalidation strategy to use dynamic rendering.

--- dmvzouk/app/page.js
+++ dmvzouk/app/page.js
@@ -1,8 +1,6 @@
 import { fetchEvents } from "@/lib/sheets";
 import CalendarApp from "./CalendarApp";
-
-export const revalidate = 300; // ISR: revalidate every 5 minutes
-
+export const dynamic = "force-dynamic";
 export default async function Page() {
   const events = await fetchEvents();
   return <CalendarApp events={events} />;
